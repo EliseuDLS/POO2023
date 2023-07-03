59674c26f81c4e50380b513862ebfb99c7cce7e6
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-07-03 17:21:27 -0300

    Update Atv08.java

--- 1°Bimestre/Atv08/notebook/Atv08.java
+++ 1°Bimestre/Atv08/notebook/Atv08.java
@@ -1,134 +1,95 @@
-//importação da classe Scanner do pacote java.util
-import java.util.Scanner; 
-
+import java.util.Scanner;
 public class Atv08 {
+    private static boolean[] assentos = new boolean[11];  // Array para representar os assentos
+    private static Scanner scanner = new Scanner(System.in);  // Scanner para ler entrada do usuário
 
-    //prepara a entrada de dados
-    final static Scanner LER = new Scanner(System.in); 
-    //declaração e alocação do vetor assentos 
-    final static String nomesClientes[] = new String[10]; 
-    //declaração e alocação do vetor assentos 
-    final static boolean assentos[] = new boolean[10];
+    public static void main(String[] args) {
+        while (true) {  // Loop principal do programa
+            exibirMenu();  // Exibe o menu de opções
+            int opcao = scanner.nextInt();  // Lê a opção escolhida pelo usuário
 
-    public static void CompraAssento(){
-		System.out.println("Por favor digite (1) para Primeira Classe ou digite (2) para Classe Econômica");
+            // Verifica a opção escolhida e reserva o assento correspondente
+            if (opcao == 1) {
+                reservarAssento(true); // Primeira Classe
+            } else if (opcao == 2) {
+                reservarAssento(false); // Classe Econômica
+            } else {
+                System.out.println("Opção inválida. Por favor, escolha novamente.");
+            }
 
-        //entrada para a classe de compra
-        int entrada = LER.nextInt();
-
-        if (entrada == 1){ 
-            int i = 0; // variavel para percorrer o vetor dos assentos
-            boolean achouAssento = true; //variavel auxiliar para percorrer assentos e achar um assento false
-
-            do { //para percorrer pelo menos uma vez
-                if (i == 5){ //apenas 5 assentos da primeira classe
-                    System.out.println("Todos os assentos da primeira classe estão cheios :( ");
-
-                    for (int hj = 5; hj<11; hj++){ //percorrendo o vetor na classe economica
-                        if (assentos[hj] == false){
-                            System.out.println("Possuímos assentos disponíveis na classe econômica :) ");
-                            System.out.println("Deseja comprar outro assento? Sim(1) Não(2)");
-
-                            int a = LER.nextInt(); //entrada para a resposta 
-
-                            if (a == 1){
-                                CompraAssento(); //chama a função para a compra novamente
-                            } else {
-                                System.out.println("Não foi possível realizar a compra :( ");
-                            }
-
-                            break;
-                        }
-                    }
-                    
-                    break;
-                }
-
-                if (assentos[i] == false){ //se achou um assento vazio (false)
-                    System.out.println("Por favor digite seu nome para efetuar a compra: "); 
-                    String nomeCliente = LER.next(); //entrada do nome do cliente em String
-                    nomesClientes[i] = nomeCliente; //adicionando no vetor o nome do cliente
-                    assentos[i] = true; //colocando que o assento foi vendido
-                    achouAssento = false; //achou um assento para de percorrer 
-
-                //vai para a proxima posição (proximo assento)
-                } else {
-                    i += 1; 
-                }
-
-            //repete se nao achou um assento
-            } while (achouAssento); 
-        }
-
-        if (entrada == 2){ 
-            int i = 5; //variavel que começa em 5 (assentos de 5 a 10)
-            boolean achouAssento = true; //auxiliando para achar um assentos
-
-            do { //repete pelo menos uma vez
-                if (i == 10){ //apenas 5 assentos da classe economica
-                    System.out.println("Todos os assentos da classe econômica estão cheios :( ");
-
-                    for (int hj = 0; hj<6; hj++){ //for de 0 até 5 (primeira classe)
-                        if (assentos[hj] == false){
-                            System.out.println("Possuímos assentos disponiveis na primeira classe! :) ");
-                            System.out.println("Deseja comprar outro assento? Sim(1) Não(2)");
-
-                            int a = LER.nextInt();
-
-                            if (a == 1){
-                                CompraAssento();
-                            } else {
-                                System.out.println("Não foi possível realizar a compra :( ");
-                            }
-                            break;
-                        }
-                    }
-			
-                    System.out.println("Infelizmente não há assentos disponiveis :( ");
-
-                    break;
-                }
-
-                if (assentos[i] == false){ //se achou um assento vazio (false)
-                    System.out.println("Por favor digite seu nome para efetuar a compra: "); 
-                    String nomeCliente = LER.next(); //entrada do nome do cliente em String
-                    nomesClientes[i] = nomeCliente; //adicionando no vetor o nome do cliente
-                    assentos[i] = true; //colocando que o assento foi vendido
-                    achouAssento = false; //achou um assento para de percorrer 
-                } else {
-                    i += 1;
-                }
-
-            } while(achouAssento);
+            // Verifica se todos os assentos foram ocupados
+            if (verificarAssentosOcupados()) {
+                System.out.println("Todos os assentos foram ocupados. O próximo voo parte em 3 horas.");
+                break;  // Sai do loop principal
+            }
         }
     }
 
-    //primeira classe a ser executada no código
-	public static void main(String[] args) {
+    // Exibe o menu de opções
+    private static void exibirMenu() {
+        System.out.println("Por favor, digite 1 para Primeira Classe ou digite 2 para Classe Econômica:");
+    }
 
-        System.out.println("Bem-vindo ao novo sistema automatizado de reservas! :)");
-        
-         //repete 10x (10 assentos)
-        for (int i = 0; i<10; i++){
+    // Reserva um assento na classe indicada (primeira classe ou classe econômica)
+    private static void reservarAssento(boolean primeiraClasse) {
+        int assento;
 
-            System.out.println("A seguir os assentos livres e ocupados: ");
-            System.out.println("Primeira classe: ");
-
-             //repete 10x (10 assentos)
-            for (int a = 0; a<10; a++){
-
-                //listando os assentos disponiveis
-                System.out.println(assentos[a]+ "\t"+ nomesClientes[a]); 
-                if (a == 4){
-                System.out.println("Classe econômica: ");
+        if (primeiraClasse) {  // Se for primeira classe
+            assento = obterProximoAssentoDisponivel(1, 5);  // Obtém o próximo assento disponível
+            if (assento == -1) {  // Se não houver assentos disponíveis
+                System.out.println("A Primeira Classe está lotada. Deseja aceitar um assento na Classe Econômica? (S/N)");
+                String resposta = scanner.next();  // Lê a resposta do usuário
+                if (resposta.equalsIgnoreCase("S")) {
+                    reservarAssento(false); // Classe Econômica
                 }
-        }
-            //chama o método para comprar os assentos
-            CompraAssento();
+            } else {
+                exibirCartaoEmbarque(assento, "Primeira Classe");  // Exibe o cartão de embarque
+            }
+        } else {  // Se for classe econômica
+            assento = obterProximoAssentoDisponivel(6, 10);  // Obtém o próximo assento disponível
+            if (assento == -1) {  // Se não houver assentos disponíveis
+                System.out.println("A Classe Econômica está lotada. Deseja aceitar um assento na Primeira Classe? (S/N)");
+                String resposta = scanner.next();  // Lê a resposta do usuário
+                if (resposta.equalsIgnoreCase("S")) {
+                    reservarAssento(true); // Primeira Classe
+                }
+            } else {
+                exibirCartaoEmbarque(assento, "Classe Econômica");  // Exibe o cartão de embarque
+            }
         }
 
-        //mensagem para o usuário
-        System.out.println("Todas os assentos lotados :( Sinto muito!");
-        System.out.println("Programa finalizado, tchau");
-	}
+        if (assento != -1) {
+            assentos[assento] = true;  // Marca o assento como ocupado
+        }
+    }
+
+    // Obtém o próximo assento disponível dentro do intervalo especificado
+    private static int obterProximoAssentoDisponivel(int inicio, int fim) {
+        for (int i = inicio; i <= fim; i++) {
+            if (!assentos[i]) {
+                return i;  // Retorna o assento disponível encontrado
+            }
+        }
+        return -1; // Assento não disponível
+    }
+
+    // Exibe o cartão de embarque com o número do assento e a classe
+    private static void exibirCartaoEmbarque(int numeroAssento, String classe) {
+        System.out.println("=======================================");
+        System.out.println("CARTÃO DE EMBARQUE");
+        System.out.println("Assento: " + numeroAssento);
+        System.out.println("Classe: " + classe);
+        System.out.println("=======================================");
+        System.out.println();
+    }
+
+    // Verifica se todos os assentos estão ocupados
+    private static boolean verificarAssentosOcupados() {
+        for (int i = 1; i <= 10; i++) {
+            if (!assentos[i]) {
+                return false;  // Retorna falso se encontrar um assento disponível
+            }
+        }
+        return true;  // Retorna verdadeiro se todos os assentos estiverem ocupados
+    }
 }
